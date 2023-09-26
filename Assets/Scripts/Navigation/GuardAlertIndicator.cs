436ae64d983b2a8faad8cc94113d9097bb663301
Likam07 <georgemalik29@gmail.com>
2023-09-26 18:03:50 -0400

    Patrol & Basic Guard Navigation
    Hello, It's Hunter. I am helping Malik. Here is what we did:
    
    Create Patrol script (SecurityPatrol.cs) where guard patrols (drunk walk) or chases player
    Crated Alert (guardalertindicator.cs) which activates itself as a child of the guard for that classic exclamation point. play a sound effect onEnable to get that without scripting
    
    patrol does not stay limited to area, guard will wander away
    patrol assumes only one player
    
    Next step is to get guards to move between a list of transforms instead of random spots around them.

---
+++ Assets/Scripts/Navigation/GuardAlertIndicator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Navigation
+{
+    public class GuardAlertIndicator : MonoBehaviour
+    {
+        private SecurityPatrol _patrol;
+        private bool _active = true;
+        private void Awake()
+        {
+            //getcomponentinparent searches self and all parents until finds one.
+            _patrol = GetComponentInParent<SecurityPatrol>();
+            if (_patrol == null)
+            {
+                Debug.LogError("Alert must be a child of SecurityPatrol! put this a a child object. Deleting.");
+                Destroy(gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            if (_patrol.patrolState == SecurityPatrol.PatrolState.ChasePlayer)
+            {
+                if (!_active)
+                {
+                    SetChildrenActive(true);
+                }
+            }
+            else
+            {
+                if (_active)
+                {
+                    SetChildrenActive(false);  
+                }
+                
+            }
+        }
+
+        private void SetChildrenActive(bool isActive)
+        {
+            _active = isActive;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(isActive);
+            }
+        }
+    }
+}
