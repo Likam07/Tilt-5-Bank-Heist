3c9d36741d99ddd216e93c45cad9ef08e409d480
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2023-09-29 16:55:38 -0400

    qwweq

--- Assets/Scripts/Scene Interactables/LazerMovement.cs
+++ Assets/Scripts/Scene Interactables/LazerMovement.cs
@@ -10,11 +10,13 @@ public class LazerMovement : MonoBehaviour
     private Vector3 initPos;
     void Start()
     {
+        //Get the initial position of the lazer
         initPos = transform.position;
     }
     
     void Update()
     {
+        //Moves the lazer up and down vertically.
         transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, initPos.z);
     }
 }
